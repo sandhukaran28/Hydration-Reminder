27307f2b5cc084508fcb9daa7393f804433b25a1
sandhukaran28 <60167401+sandhukaran28@users.noreply.github.com>
2021-04-11 08:29:27 +0530

    initial commit

---
+++ app/src/main/java/com/example/android/background/sync/ReminderTasks.java
@@ -0,0 +1,18 @@
+package com.example.android.background.sync;
+
+import android.content.Context;
+
+import com.example.android.background.utilities.PreferenceUtilities;
+
+public class ReminderTasks{
+    public static final String ACTION_INCREMENT_WATER_COUNT="increment-water-count";
+
+
+private static void incrementWaterCount(Context context){
+    PreferenceUtilities.incrementWaterCount(context);
+}
+public static void executeTask(Context context,String action){
+    if(action==ACTION_INCREMENT_WATER_COUNT)
+        incrementWaterCount(context);
+}
+}
